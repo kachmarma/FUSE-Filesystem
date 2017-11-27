47687303972c23f2997bf4968fa90672fccdfb4f
Julian <dryx5@protonmail.com>
2017-11-27 16:26:22 -0500

    moved structs from storage.c to storage.h

--- ch03-assignment/storage.c
+++ ch03-assignment/storage.c
@@ -1,53 +1,10 @@
-
 #include <stdio.h>
 #include <string.h>
 
 #include "storage.h"
 
-// flags for file Data
-typedef struct myFlag {
+superBlock sb;
 
-} myFlag;
-
-// important file/directory Data
-typedef struct file_data {
-    const char* path;
-    int         mode;
-    int         uid;
-    int         dataSize;
-    int         accessTime;
-    int         createTime;
-    int         modTime;
-    int         groupID;
-    int         ref_count;
-    int         blockCount;
-    myFlag      flag;
-} file_data;
-
-// points to 1024 data Blocks
-typedef struct indiBlock {
-    void* dBlocks[1024];
-} indiBlock;
-
-// represents an iNode containing important file data and pointers to data Blocks
-typedef struct myNode {
-    file_data data;        // contains important file data
-    void* dBlocks[12];     // assigned directly to data blocks
-    indiBlock* indiBlock;  // pointer to indirect blocks
-} myNode;
-
-// bitmap
-typedef struct bmap {
-
-} bmap;
-
-// contains important file system pointers
-typedef struct superBlock {
-    bmap* myNodesMap; // bit map of free and used data blocks
-    bmap* dataBlockMap; // bit map of free and used inodes
-    myNode* rootMyNode; // the root inode
-    void* myNodeTable; // the table of 256 myNodes
-} superBlock;
 
 static file_data file_table[] = {
     {"/", 040755, 0},
@@ -115,3 +72,8 @@ get_data(const char* path)
     return dat->data;
 }
 
+void
+initSB()
+{
+
+}
